0882d38cfb2ee34d8aa3245064f041c484942af9
Julian Se <julianpse@gmail.com>
2018-03-04 19:41:18 +0000

    added list_exercises 1-5

---
+++ list_exercises.py
@@ -0,0 +1,40 @@
+list_of_numbers = [-21,1,14,-8,3,7,9,5,2,0,24,12,46]
+
+#1. Sum the Numbers
+print(sum(list_of_numbers))
+
+#2. Largest Number
+print(max(list_of_numbers))
+
+#3. Smallest Number
+print(min(list_of_numbers))
+
+#4. Even Numbers
+for numbers in list_of_numbers:
+    if (numbers % 2 == 0):
+        print(numbers)
+
+#5. Positive Numbers
+for numbers in list_of_numbers:
+    if (numbers > 0):
+        print(numbers)
+
+#6. Positive Numbers 2
+
+
+#7. Multiply a List
+
+
+#8. Multiply Vectors
+
+
+#9. Matrix Addition
+
+
+#10. Matrix Addition II
+
+
+#11. De-Dup
+
+
+
