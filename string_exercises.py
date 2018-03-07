97715fcf9fe210eeefe684a4227c36bd7ed716a0
Julian Se <julianpse@gmail.com>
2018-03-07 03:20:10 +0000

    new stuff

--- string_exercises.py
+++ string_exercises.py
@@ -1,5 +1,5 @@
 string = "hello how are you."
-
+"""
 #1. Uppercase a String
 print(string.upper())
 
@@ -8,9 +8,24 @@ print(string.capitalize())
 
 #3. Reverse a string
 print(string[::-1])
-
+"""
 #4. Leetspeak not done
-paragraph = "Given a paragraph of text as a string, print the paragraph in leetspeak."
+def leet_translator():
+    leet_dictionary = {"a": 4, "e" : 3, "g" : 6, "i" : 1, "o": 0, "s": 5, "t" : 7}
+    user_input = input("Enter your string here: ").split()
+    out = [ ]
+    
+    for letters in user_input:
+        if letters in leet_dictionary:
+            print (leet_dictionary[letters])
+        else:
+            while leet_dictionary[letters] == False:
+                out = out.append(letters)
+        print(out)
+
+leet_translator()       
+        
+"""
 
 
 #5. not done
@@ -26,9 +41,12 @@ print(string)
 #6  Caesar Cipher
 import codecs
 secret_message = ""
+
 def caesar_cipher(secret_message):
     secret_message = codecs.encode("{}".format(secret_message), 'rot_13')
     print(secret_message)    
   
    
 caesar_cipher("lbh zhfg hayrnea jung lbh unir yrnearq")
+
+"""
