cff86a00538a9ff66c633c6182c60bfa5e23e5c5
Julian Se <julianpse@gmail.com>
2018-03-11 00:18:16 +0000

    started phonebook

---
+++ phonebook.py
@@ -0,0 +1,14 @@
+phonebook = {
+    "Ellen Jacobs" : "281-555-5555",
+    "Johnny Tsunami" : "832-999-5555",
+    "Mike Jones" : "281-330-8004",
+    "Christopher Chan" : "713-330-8004",
+    "Avinash Ravishankar" : "281-888-9999",
+    "Riley Robertson" : "222-222-1102",
+    "Pizza Hut" : "281-999-5555"
+}
+
+def look_up_number():
+    pass
+
+
